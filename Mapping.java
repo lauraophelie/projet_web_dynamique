8d7b40c1af329ce1a792d90f11b836add0bd8194
Laura Ophélie <lauraophelie1@gmail.com>
2023-03-07 11:20:55 +0100

    sprint2

---
+++ Mapping.java
@@ -0,0 +1,23 @@
+package etu1885.framework;
+
+public class Mapping {
+    String className;
+
+    public String getClassName() {
+        return this.className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    String method;
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+}
